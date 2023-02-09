ce79d47c8f39d15a3220f70c032a47a7392b93e0
groupincworld <118275411+joyyourday@users.noreply.github.com>
2023-02-09 06:18:28 +0530

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,25 @@
 import '@styles/globals.css'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ShareThis from '../components/ShareThis';
 
+import Script from 'next/script';
 function Application({ Component, pageProps }) {
   return  (
        <div>
+       
   <Header />
   <Component {...pageProps} />
   <Footer />
+  <ShareThis/>
+
+  <Script
+        src="(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',5581590,document.body||document.documentElement)"
+       
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
   </div>
   )
 }
