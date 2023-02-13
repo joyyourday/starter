a770a16f2f397f7883c8c781fb5d92fccd5550a4
groupincworld <118275411+joyyourday@users.noreply.github.com>
2023-02-13 08:00:33 +0530

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,26 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    theme: {
+      opacity: {
+        '0': '0',
+        '20': '0.2',
+        '40': '0.4',
+        '60': '0.6',
+        '80': '0.8',
+        '100': '1',
+      },
+      
+      extend: {
+        screens: {
+          '3xl': '1600px',
+        },
+
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+
+    require('tw-elements/dist/plugin')
+  ],
 }
